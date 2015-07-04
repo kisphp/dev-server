c66909c48a28b978e0f0d80d5b13f8ad2d01277e
Marius Bogdan <marius.rizac@spryker.com>
2015-07-04 19:50:34 +0200

    updated bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,11 +3,7 @@
 apt-get update
 apt-get install -y vim mc tree git
 
-git clone https://kisphp@bitbucket.org/kisphp/ubuntu_server_install.git libs/kisphp/ubuntu_server_install
-
 if ! [[ -L /var/www ]]; then
     rm -rf /var/www
     ln -fs /home/web /var/www
 fi
-
-. libs/kisphp/ubuntu_server_install/install.sh
