2f06d6c9e4f6f22a20a1652873fddbf49b263646
Marius Bogdan <marius.rizac@spryker.com>
2015-11-12 22:59:53 +0100

    added support for npm and nodejs

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,14 +6,24 @@ echo " "
 
 apt-get update
 apt-get upgrade
-apt-get -y install vim mc tree git python-pip python-setuptools
+apt-get -y install vim mc tree git python-pip python-setuptools build-essential
 ln -s /sbin/ifconfig /bin/ifconfig
 sudo apt-get -y install zsh
 
+echo " Install phpunit "
 wget https://phar.phpunit.de/phpunit.phar
 chmod +x phpunit.phar
 mv phpunit.phar /usr/local/bin/phpunit
 phpunit --version
+
+echo " Install NodeJS "
+apt-get install -y nodejs npm
+ln -s /usr/bin/nodejs /usr/bin/node
+
+echo " Update nodejs and npm "
+npm cache clean -f
+npm install -g n
+n stable
 
 #echo " INSTALL OH-MY-ZSH "
 #sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
